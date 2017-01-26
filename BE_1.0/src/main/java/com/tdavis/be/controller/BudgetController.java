f32d943439b449af19850630f7fb1a2979f8440e
luckybob34 <luckybob34@gmail.com>
2017-01-26 06:29:07 -0600

    update

--- BE_1.0/src/main/java/com/tdavis/be/controller/BudgetController.java
+++ BE_1.0/src/main/java/com/tdavis/be/controller/BudgetController.java
@@ -2,6 +2,8 @@ package com.tdavis.be.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,12 +17,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tdavis.be.entity.Budget;
 import com.tdavis.be.service.BudgetService;
+import com.tdavis.be.service.ProjectService;
 
 @Controller
 @RequestMapping("/budget")
 public class BudgetController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private BudgetService budgetService;
+	
+	@Autowired
+	private ProjectService projectService;
 	
 	@ModelAttribute("budget")
 	public Budget construct() {
@@ -43,16 +51,24 @@ public class BudgetController {
 			
 			return "project";
 		}
+		int id = budget.getProject().getId();
+		logger.info("id= " +id);
+		budgetService.save(budget);
 		
-		//budgetService.save(budget);
+		double approved = projectService.findById(id).getApproved_budget();
+		double spent = projectService.findById(id).getSpent_budget();
+		double balance = 0;
+		
+		balance = approved - spent;
 		
     	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
     	String name = auth.getName(); //get logged in username
     	model.addAttribute("username", name);
-    	model.addAttribute("projects", budgetService.findById(budget.getProject().getId()));
-		model.addAttribute("title", "Projects");		
+    	model.addAttribute("project", projectService.findById(id));
+		model.addAttribute("title", "Projects");
+		model.addAttribute("spent",balance);
 		model.addAttribute("success", true);
 		//Change to budget-detail when built
-		return "project-detail";
+		return "project_details";
 	}
 }
