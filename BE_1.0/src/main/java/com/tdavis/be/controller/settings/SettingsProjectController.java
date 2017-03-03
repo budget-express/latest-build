95e458c3e72a30b1c4f1a12de19a508b2f835187
luckybob34 <luckybob34@gmail.com>
2017-03-02 22:58:11 -0600

    updates

---
+++ BE_1.0/src/main/java/com/tdavis/be/controller/settings/SettingsProjectController.java
@@ -0,0 +1,156 @@
+package com.tdavis.be.controller.settings;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.tdavis.be.entity.Project;
+import com.tdavis.be.service.ProjectService;
+
+
+
+@Controller
+@RequestMapping("/settings")
+public class SettingsProjectController {
+	//Log output to console
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	//Constants
+	private String redirectProjects = "redirect:/settings/projects";
+	
+	@Autowired
+	private ProjectService projectService;	
+	
+	//Access project on web page
+	@ModelAttribute("project")
+	public Project projectConstruct() {
+		return new Project();
+	}
+	
+	/********************************************************************************************************
+	 *  /Settings/Projects - List(main page), Find/Sort Projects
+	 * 	Query Project Objects
+	 * 
+	 *********************************************************************************************************/
+	
+	/*
+	 * Home >> Settings >> Projects >> All
+	 * Listing of Projects
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping("/projects")
+	public String listProjects(Model model) {
+		
+		//Set Page Navigation
+		List<String> navigation = new ArrayList<>();
+		
+		navigation.add("Home");
+		navigation.add("Settings");
+		navigation.add("Projects");
+		
+		//Find Projects sort by Year (page)
+		Page<Project> page = projectService.getProjectByYear(1);
+		
+		//Find logged in user
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    	String name = auth.getName(); //get logged in username
+    	
+    	//Set Model Attributes
+    	model.addAttribute("username", name);
+    	model.addAttribute("projects", page);
+    	model.addAttribute("navtitle", "Projects");
+    	model.addAttribute("navigation" , navigation);
+		model.addAttribute("title", "Settings>>Projects");
+		
+		//projects.html
+		return "projects";
+	}
+	
+	/*
+	 * Home >> Settings >> Projects >> All
+	 * Listing of Projects
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping("/projects/{sort}")
+	public String listProjectsAll(RedirectAttributes redir, @PathVariable String sort) {
+		logger.info("Sort by: " + sort);
+		switch (sort) {
+			case "all":
+				redir.addAttribute("projects", projectService.getProjectByYear(1));
+				break;
+			case "active":
+			case "planning":
+			case "closed":
+				redir.addAttribute("projects", projectService.getProjectByStatus(sort,1));
+				break;
+			default:
+				redir.addFlashAttribute("projects", projectService.findAll());
+				break;
+		}
+		logger.info("Exit");
+		
+    	
+		return redirectProjects;
+	}
+	
+	/********************************************************************************************************
+	 *  /Settings/Projects - Save, Delete
+	 * 	Interact with User/Role Repositories - Access Database
+	 * 
+	 *********************************************************************************************************/
+	
+	/*
+	 * Home >> Settings >> Projects >> Save
+	 * Save Project
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping("/project/save")
+	public String saveProject(@ModelAttribute @Valid Project project, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			
+			logger.debug("Error: "+bindingResult);
+			return "redirect:/index2";
+		}
+		
+		//Call Project Service to Save Project
+		projectService.save(project);
+		
+		//Redirect to Projects
+		return "redirect:/settings/projects";
+	}
+	
+	/*
+	 * Home >> Settings >> Projects >> Delete
+	 * Delete Project
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping("/project/delete/{id}")
+	@ResponseBody
+	public String projectDelete(Model model, @PathVariable int id){
+
+		//Call Project Service to Delete Project
+		projectService.delete(id);
+
+		//Redirect to Projects
+		return "success";
+	}	
+}
---
+++ BE_1.0/src/main/java/com/tdavis/be/controller/settings/SettingsUserController.java
@@ -0,0 +1,160 @@
+package com.tdavis.be.controller.settings;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tdavis.be.entity.User;
+import com.tdavis.be.service.UserService;
+
+
+
+@Controller
+@RequestMapping("/settings")
+public class SettingsUserController {
+	
+	//Log output to console
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	//Constants
+	private String redirectUsers = "redirect:/settings/users";
+
+	@Autowired
+	private UserService userService;
+	
+	//Access user on web page
+	@ModelAttribute("user")
+	public User construct() {
+		return new User();
+	}
+	
+	//Access Roles on web page
+	@ModelAttribute("userRoles")
+	public List<String> constructRole() {
+		return new ArrayList<>();
+	}
+
+	/********************************************************************************************************
+	 *  /Settings/Users - Main Page
+	 * 	Query User/Role Objects
+	 * 
+	 *********************************************************************************************************/
+	
+	/*
+	 * List Users/Roles
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping("/users")
+	public String listUsers(Model model) {
+	
+		//Set Page Navigation
+		List<String> navigation = new ArrayList<>();
+		
+		navigation.add("Home");
+		navigation.add("Settings");
+		navigation.add("Users");
+				
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	   	String name = auth.getName(); //get logged in username
+	   	model.addAttribute("username", name);
+	   	model.addAttribute("fname", userService.findByName(name).getFname());
+	   	model.addAttribute("lname", userService.findByName(name).getLname());
+	   	model.addAttribute("navigation",navigation);
+	   	model.addAttribute("navtitle", "Users");
+	   	model.addAttribute("users", userService.findAll());
+	   	model.addAttribute("roles", userService.findAllRoles());
+		model.addAttribute("title", "Settings>>Users");
+		
+		return "users";
+	}
+	
+	/********************************************************************************************************
+	 *  /Settings/Users - Save, Enable, Delete
+	 * 	Interact with User/Role Repositories - Access Database
+	 * 
+	 *********************************************************************************************************/
+	
+	/*
+	 * Save User
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping("/user/save")
+	public String saveUser2(@ModelAttribute @Valid User user, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			logger.error(bindingResult+"");
+			return "/error";
+		}
+		
+		//Call User Services to Save User
+		userService.save(user);
+		
+		//userService.updateRoles(user,userRoles);
+
+		//Set Success for Deletion
+		model.addAttribute("success", true);
+		
+		//Redirect to User Details
+		return redirectUsers;
+	}
+	
+	/*
+	 * Enable/Disable User
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping("/user/enable/{id}")
+	@ResponseBody
+	public String enableUser(Model model, @PathVariable int id) {
+		
+		//Toggle User Enabled
+		User user = userService.findById(id);
+		if (user.isEnabled()) {
+			user.setEnabled(false);
+		} else {
+			user.setEnabled(true);
+		}
+		
+		//Call User Services to Save User
+		userService.save(user);
+		
+		//Set Success for Deletion
+		model.addAttribute("success", true);
+		
+		//Redirect to User Details
+		return "success";
+	}
+	
+	/*
+	 * Delete User
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping("/user/delete/{id}")
+	@ResponseBody
+	public String deleteUser(Model model, @PathVariable int id) {
+		
+		//Call User Services to Delete User
+		userService.delete(id);
+		
+		//Set Success for Deletion
+		model.addAttribute("success", true);
+		
+		//Redirect to User Details
+		return "success";
+	}
+}
